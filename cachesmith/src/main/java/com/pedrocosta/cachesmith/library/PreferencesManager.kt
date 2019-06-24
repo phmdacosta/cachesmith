ca72f723d34e5d4b5d7162be083643e327b4855a
phmdacosta <ph.mcosta@hotmail.com>
2019-06-24 17:36:04 +0100

    Update PreferencesManager.kt
    Implementing table saving in shared preferences to validate future changes in model's class.

--- cachesmith/src/main/java/com/pedrocosta/cachesmith/library/PreferencesManager.kt
+++ cachesmith/src/main/java/com/pedrocosta/cachesmith/library/PreferencesManager.kt
@@ -5,6 +5,12 @@ import android.content.SharedPreferences
 import com.pedrocosta.cachesmith.library.config.Config
 
 object PreferencesManager {
+
+	companion object {
+        private val SHARED_PREFERENCES_NAME = "cacheSmithSharedPref"
+        private val DB_TABLES_KEY = "cacheSmithDbTablesKey"
+    }
+	
     private var mPref: SharedPreferences? = null
     private var editor: SharedPreferences.Editor? = null
 
@@ -35,5 +41,46 @@ object PreferencesManager {
             }
         }
         return value!!
+    }
+    
+    fun addDBTable(context: Context, table: DBTable) {
+    	synchronized(this) {
+            context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE).edit().also {
+            	var jsonTables: JSONArray = JSONArray(it.getString(DB_TABLES_KEY, ""))
+            	
+            	for (JSONObject jObj: jsonTables) {
+            		var jsonColumns = jObj.getJSONArray(table.name)
+            		if (jsonColumns != null && jsonColumns.size() > 0) {
+            			
+            		}
+            	}
+                it.putString(DB_TABLES_KEY, tables)
+                it.apply()
+            }
+        }
+    }
+    
+    fun getDBTables(context: Context): List {
+    	var tables: JSONArray? = null
+        value ?: synchronized(this) {
+            value ?: open(context).also {
+                val value = it.getString(DB_TABLES_KEY, "")
+                val jsonArray = JSONArray(value)
+                
+                for (i in 0 until jsonArray.length()) {
+				    val tableJson = jsonArray.getJSONObject(i)
+				    var table = tableJson.
+				}
+            }
+        }
+        return tables!!
+    }
+    
+    class DBTable(name: String) {
+    	val columns: List<String> = ArrayList<String>()
+    	
+    	fun getColumnsJson(): JSONArray {
+    		return JSONArray(Gson().toJson(columns))
+    	}
     }
 }
