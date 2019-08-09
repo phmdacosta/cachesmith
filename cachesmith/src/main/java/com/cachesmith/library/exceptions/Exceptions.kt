e814cf805950bc03fa0e92d50f9a69fead130ed5
Pedro da Costa <ph.mcosta@hotmail.com>
2019-08-09 19:59:55 +0100

    Setting a default message for NoVersionException
    Putting all exception in same file.

--- cachesmith/src/main/java/com/cachesmith/library/exceptions/Exceptions.kt
+++ cachesmith/src/main/java/com/cachesmith/library/exceptions/Exceptions.kt
@@ -4,4 +4,9 @@ import java.lang.Exception
 
 class NoVersionException(message: String, cause: Throwable?): Exception(message, cause) {
     constructor(message: String) : this(message, null)
+	constructor() : this("Could not get version for database.")
+}
+
+class SQLiteQueryException(message: String, cause: Throwable?): Exception(message, cause) {
+    constructor(message: String) : this(message, null)
 }
