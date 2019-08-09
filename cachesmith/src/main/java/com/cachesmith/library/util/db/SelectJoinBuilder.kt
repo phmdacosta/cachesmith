e01937322bfd5eb60d453d799bf64849656cb4a3
Pedro da Costa <ph.mcosta@hotmail.com>
2019-08-09 19:56:39 +0100

    Developing a new QueryBuilder to build query for select with joins.

--- cachesmith/src/main/java/com/cachesmith/library/util/db/SelectJoinBuilder.kt
+++ cachesmith/src/main/java/com/cachesmith/library/util/db/SelectJoinBuilder.kt
@@ -2,29 +2,40 @@ package com.cachesmith.library.util.db
 
 import com.cachesmith.library.util.ObjectClass
 import com.cachesmith.library.util.JoinType
+import com.cachesmith.library.exceptions.SQLiteQueryException
+import com.cachesmith.library.util.db.models.ColumnObject
+import com.cachesmith.library.util.db.models.ForeignKeyObject
 
 open class SelectJoinBuilder(val principal: ObjectClass, val joiTtype: JoinType) : SelectBuilder(principal) {
 	
 	companion object {
-		const val AS = "AS".plus(SPACE)
 		const val DOT = ".".plus(SPACE)
 	}
 	
-	var joinTables = mutableListOf<ObjectClass>()
-	val selectColumnsMap = mutableMapOf<String, String>()
+	private var joinTables = mutableListOf<ObjectClass>()
+	private val joinFilters = mutableListOf<Join>()
+	private val selectColumnsMap = mutableMapOf<String, String>()
 	
 	fun joinTable(table: ObjectClass) {
-		joinTables.add(table)
+		if (!containsTable(table.tableName))
+			joinTables.add(table)
 	}
 	
 	fun addSelectColumn(tableName: String, columnName: String) {
-		if (containsTable(tableName))
+		if (!containsTable(tableName))
+			throw SQLiteQueryException("Table $tableName not added.")
 		selectColumnsMap.put(tableName, columnName)
 		selectAll = false
 	}
 	
-	fun buildTables(query: StringBuffer) {
+	fun addJoinFilter(joinType: JoinType, firstTable: ObjectClass, secondTable: ObjectClass) {
+		if (firstTable.tableName != principal.tableName && !containsTable(firstTable.tableName))
+			joinTable(firstTable)
 		
+		if (!containsTable(secondTable.tableName))
+			joinTable(secondTable)
+		
+		joinFilters.add(Join(firstTable, secondTable, joinType))
 	}
 	
 	fun containsTable(tableName: String): Boolean {
@@ -33,6 +44,34 @@ open class SelectJoinBuilder(val principal: ObjectClass, val joiTtype: JoinType)
 				return true
 		}
 		return false;
+	}
+	
+	fun buildTables(query: StringBuffer) {
+		query.append(principal)
+		query.append(SEPARATOR)
+		query.append(SPACE)
+		
+		val tablesIter = joinTables.iterator()
+		while(tablesIter.hasNext()) {
+			val tableObject = tablesIter.next()
+			query.append(tableObject.tableName)
+			if (tablesIter.hasNext()) {
+				query.append(SEPARATOR)
+				query.append(SPACE)
+			}
+		}
+	}
+	
+	fun buildJoinFilters(query: StringBuffer) {
+		val joinsIter = joinFilters.iterator()
+		while(joinsIter.hasNext()) {
+			val joinObject = joinsIter.next()
+			query.append(joinObject.build())
+			if (joinsIter.hasNext()) {
+				query.append(SEPARATOR)
+				query.append(SPACE)
+			}
+		}
 	}
 	
 	override fun buildSelectColumns(query: StringBuffer) {
@@ -47,6 +86,7 @@ open class SelectJoinBuilder(val principal: ObjectClass, val joiTtype: JoinType)
 				query.append(selectColumn.value)
 				if (selectIter.hasNext()) {
 					query.append(SEPARATOR)
+					query.append(SPACE)
 				}
 			}
 		}
@@ -54,7 +94,7 @@ open class SelectJoinBuilder(val principal: ObjectClass, val joiTtype: JoinType)
 	
 	override fun buildFilters(query: StringBuffer) {
 		if (filters.isNotEmpty()) {
-			query.append(WHERE)
+			query.append(SQLCommands.WHERE.value)
 			val filtersIter = filters.iterator()
 			while(filtersIter.hasNext()) {
 				val filter = filtersIter.next()
@@ -66,7 +106,7 @@ open class SelectJoinBuilder(val principal: ObjectClass, val joiTtype: JoinType)
 				query.append(SPACE)
 				
 				if (filtersIter.hasNext()) {
-					query.append(AND)
+					query.append(SQLCommands.AND.value)
 				}
 			}
 		}
@@ -74,11 +114,11 @@ open class SelectJoinBuilder(val principal: ObjectClass, val joiTtype: JoinType)
 	
 	override fun build(): String {
 		val query = StringBuffer()
-		query.append(SELECT)
+		query.append(SQLCommands.SELECT.value)
 		
 		buildSelectColumns(query)
 		
-		query.append(FROM)
+		query.append(SQLCommands.FROM.value)
 		query.append(entity.tableName)
 		query.append(SPACE)
 		
@@ -87,10 +127,50 @@ open class SelectJoinBuilder(val principal: ObjectClass, val joiTtype: JoinType)
 		
 		// Order by statement
 		if (!orderBy.isBlank()) {
-			query.append(ORDER_BY)
+			query.append(SQLCommands.ORDER_BY.value)
 			query.append(orderBy)
 		}
 		
 		return query.toString()
+	}
+	
+	private class Join(val first: ObjectClass, val second: ObjectClass, val type: JoinType) {
+		fun build(): String {
+			val filter = StringBuffer()
+			when(type) {
+				JoinType.CROSS_JOIN -> filter.append(SQLCommands.CROSS_JOIN.value)
+				JoinType.INNER_JOIN -> filter.append(SQLCommands.INNER_JOIN.value)
+				JoinType.OUTER_JOIN -> filter.append(SQLCommands.OUTER_JOIN.value)
+			}
+			filter.append(SPACE)
+			filter.append(SQLCommands.ON.value)
+			filter.append(SPACE)
+			filter.append(first.tableName)
+			filter.append(DOT)
+			
+			var foreignKey: ForeignKeyObject? = null
+			first.fields.forEach {
+				val column = ColumnObject(it)
+				if (column.isForeignKey && column.foreignKey.referenceTable.equals(second.tableName)) {
+					foreignKey = column.foreignKey
+					filter.append(column.name)
+				}				
+			}
+			
+			if (foreignKey == null) {
+				throw SQLiteQueryException("Could not find foreign key of ${second.tableName} in ${first.tableName}")
+			}
+			
+			filter.append(EQUALS)
+			filter.append(second.tableName)
+			filter.append(DOT)
+			
+			second.fields.forEach {
+				val column = ColumnObject(it)
+				if (column.isPrimaryKey && foreignKey!!.referenceColumn.equals(column.name))
+					filter.append(column.name)
+			}
+			return filter.toString()
+		}
 	}
 }
